a0ab77afc31225314cc730c68bb155dd9d8af7e7
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-09 13:04:49 +0100

    import R's ahead

--- src/Ahead.jl
+++ src/Ahead.jl
@@ -1,5 +1,20 @@
-module Ahead
+module Ahead		
+	
+	import RCall as rc 
 
-# Write your package code here.
+	rc.reval("options(repos = c(
+    techtonique = 'https://techtonique.r-universe.dev',
+    CRAN = 'https://cloud.r-project.org'));    
+	install.packages("ahead")")	
+
+	@rimport base as rbase
+	@rimport ahead as rahead
+
+	function foo(x)
+		rbase.sum([1, 2, 3], var"rm.na" = true)
+	end 
+
+	# exports 
+	export foo 
 
 end
