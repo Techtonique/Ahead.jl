bc23a14fe0c0e7c82509fed7e68249a24f396166
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-11 20:00:43 +0100

    use Sys.getenv

--- src/Ahead.jl
+++ src/Ahead.jl
@@ -29,8 +29,8 @@ module Ahead
 	end
 	
 	R"install_ahead <- try(utils::install.packages('ahead', repos = c('https://techtonique.r-universe.dev', 'https://cloud.r-project.org'), dependencies=TRUE), silent = TRUE)"
-	R"if(inherits(install_ahead, 'try-error')) {dir.create(sys.getenv('r_libs_user'), recursive = true); 
-	.libpaths(sys.getenv('r_libs_user'))
+	R"if(inherits(install_ahead, 'try-error')) {dir.create(Sys.getenv('r_libs_user'), recursive = true); 
+	.libpaths(Sys.getenv('r_libs_user'))
 	; utils::install.packages('ahead', dependencies=TRUE)}"	
 	
 	R"load_ahead <- try(library(ahead), silent = TRUE)"
