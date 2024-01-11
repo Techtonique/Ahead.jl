59b4123c9323460a2f75e21f94a29cad9cb4db58
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-11 20:33:40 +0100

    install from 'r_pkgs' Pt.4

--- src/Ahead.jl
+++ src/Ahead.jl
@@ -29,7 +29,7 @@ module Ahead
 	end
 	
 	R"install_ahead <- try(utils::install.packages('ahead', repos = c('https://techtonique.r-universe.dev', 'https://cloud.r-project.org'), dependencies=TRUE), silent = TRUE)"
-	R"if(inherits(install_ahead, 'try-error')) {dir.create('r_libs_user'), recursive = TRUE); 
+	R"if(inherits(install_ahead, 'try-error')) {dir.create('r_libs_user', recursive = TRUE); 
 	.libPaths('r_libs_user')
 	; utils::install.packages('ahead', repos = c('https://techtonique.r-universe.dev', 'https://cloud.r-project.org'), dependencies=TRUE)}"	
 	
