4a424dd1ac518e98d7c20873dea91cfea728ef22
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-11 22:50:25 +0100

    install foreach, Rcpp, snow Pt.3

--- src/Ahead.jl
+++ src/Ahead.jl
@@ -28,10 +28,12 @@ module Ahead
 		run(`sudo usermod -aG staff $username`)
 	end
 	
-	R"install_ahead <- try(utils::install.packages(c('foreach', 'Rcpp', 'snow', 'ahead'), repos = c('https://techtonique.r-universe.dev', 'https://cloud.r-project.org'), dependencies=TRUE), silent = TRUE)"
-	R"if(inherits(install_ahead, 'try-error')) {dir.create('r_libs_user', recursive = TRUE); 
-	.libPaths('r_libs_user')
-	; utils::install.packages(c('foreach', 'Rcpp', 'snow', 'ahead'), repos = c('https://techtonique.r-universe.dev', 'https://cloud.r-project.org'), dependencies=TRUE)}"	
+	R"try(utils::install.packages(c('foreach', 'Rcpp', 'snow'), dependencies=TRUE), silent = TRUE)"
+	
+	R"install_ahead <- try(utils::install.packages('ahead', dependencies=TRUE), silent = TRUE)"
+	
+	R"if(inherits(install_ahead, 'try-error')) {dir.create('r_libs_user'); 
+	.libPaths('r_libs_user'); utils::install.packages('ahead', repos = c('https://techtonique.r-universe.dev', 'https://cloud.r-project.org'), lib='r_libs_user', dependencies=TRUE)}"	
 	
 	R"load_ahead <- try(library(ahead), silent = TRUE)"
 	R"if(inherits(install_ahead, 'try-error')) {utils::install.packages('https://techtonique.r-universe.dev/src/contrib/ahead_0.9.0.tar.gz', repos = NULL, type = 'source', dependencies = TRUE); library(ahead)}"
