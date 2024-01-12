41b8b3293931ec1cd9bd15fbd959df05c6ec6a80
thierrymoudiki <thierry.moudiki@gmail.com>
2024-01-12 09:05:55 +0100

    run tests Pt.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Ahead
 using Test
 
 @testset "Ahead.jl" begin
-	val = Ahead.foo(1)
-    print(val['residuals'][1])
-    @test isapprox(val['residuals'][1], 0)
+	val = Ahead.foo(1)    
+    print(val)
+    @test val == 0
 end
